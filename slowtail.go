4c543827518b9ecca5b3854095224c59c28a0b5d
Serge Bedzhyk <smileart21@gmail.com>
2016-12-29 14:00:34 +0200

    fix: delete excessive isStdin argument refactor: reorder/rename functions

--- slowtail.go
+++ slowtail.go
@@ -11,13 +11,13 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/docopt/docopt-go"
-	"github.com/fatih/color"
-	"github.com/hpcloud/tail"
-	"github.com/jprichardson/readline-go"
+	docopt "github.com/docopt/docopt-go"
+	color "github.com/fatih/color"
+	tail "github.com/hpcloud/tail"
+	readline "github.com/jprichardson/readline-go"
 )
 
-const version = "Slow Tail v1.0"
+const version = "Slow Tail v0.1"
 
 const doc = `Slow Tail 🐕
 
@@ -32,13 +32,112 @@ const doc = `Slow Tail 🐕
                            Keep in mind: you can't rewind STDIN but you can skip <n>
                            lines from the beginning using this option`
 
-type options struct {
+type arguments struct {
 	rewindLines       int
 	delayMilliseconds int
 	filePath          string
 }
 
 var globalDelay = 0
+
+func main() {
+	args, _ := docopt.Parse(doc, nil, true, version, false)
+
+	options, err := parseArgs(args)
+
+	if err != nil {
+		checkErr(err)
+	}
+
+	globalDelay = options.delayMilliseconds
+
+	if len(options.filePath) > 0 {
+		linesChannel := make(chan string, 1)
+
+		if isStdin() {
+			go stdinToChan(os.Stdin, &linesChannel, options.rewindLines)
+		} else {
+			go fileToChan(options.filePath, &linesChannel, options.rewindLines)
+		}
+
+		for line := range linesChannel {
+			fmt.Println(line)
+		}
+	}
+}
+
+func parseArgs(args map[string]interface{}) (arguments, error) {
+
+	rewindLines := int(0)
+	delayMilliseconds := int(250)
+	filePath := ""
+
+	if rewindArg, ok := args["--rewind"].(string); ok {
+		rewindLines, _ = strconv.Atoi(rewindArg)
+	}
+
+	if rewindLines < 0 || rewindLines > math.MaxInt32 {
+		return arguments{}, errors.New("--rewind must be a positive number of lines")
+	}
+
+	if delayArg, ok := args["--delay"].(string); ok {
+		delayMilliseconds, _ = strconv.Atoi(delayArg)
+	}
+
+	if delayMilliseconds < 0 || delayMilliseconds > math.MaxInt32 {
+		return arguments{}, errors.New("--delay must be a positive number of milliseconds")
+	}
+
+	if filePathArg, ok := args["<file>"].(string); ok {
+		filePath = filePathArg
+	}
+
+	return arguments{
+		rewindLines,
+		delayMilliseconds,
+		filePath,
+	}, nil
+}
+
+func stdinToChan(source io.Reader, linesChannel *chan string, rewindLinesCount int) {
+	defer close(*linesChannel)
+
+	readline.ReadLine(os.Stdin, func(line string) {
+		*linesChannel <- line
+
+		if rewindLinesCount == 0 {
+			sleepMilliseconds(globalDelay)
+		}
+
+		if rewindLinesCount > 0 {
+			rewindLinesCount--
+		}
+	})
+}
+
+func fileToChan(source string, linesChannel *chan string, rewindLinesCount int) {
+	defer close(*linesChannel)
+
+	if rewindLinesCount > 0 {
+		tailFile(source, rewindLinesCount)
+	}
+
+	t, err := tail.TailFile(source, tail.Config{
+		Follow:   true,
+		Poll:     true,
+		Location: &tail.SeekInfo{Offset: 0, Whence: 2},
+		Logger:   tail.DiscardingLogger,
+	})
+
+	if err == nil {
+		for line := range t.Lines {
+			*linesChannel <- line.Text
+			sleepMilliseconds(globalDelay)
+		}
+	} else {
+		checkErr(err)
+	}
+}
 
 func eachFileLine(filePath string, callback func(lineNum int, line string) error) (linesRead int, err error) {
 	file, err := os.Open(filePath)
@@ -86,13 +185,7 @@ func tailFile(filePath string, linesCount int) {
 	}
 }
 
-func checkErr(err error) {
-	if err != nil {
-		log.Fatal(color.RedString("ERROR: "), color.RedString(err.Error()))
-	}
-}
-
-func isStdinPath(filePath string) bool {
+func isStdin() bool {
 	stat, _ := os.Stdin.Stat()
 	if (stat.Mode() & os.ModeCharDevice) == 0 {
 		return true
@@ -101,104 +194,12 @@ func isStdinPath(filePath string) bool {
 	return false
 }
 
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(color.RedString("ERROR: "), color.RedString(err.Error()))
+	}
+}
+
 func sleepMilliseconds(ms int) {
 	time.Sleep(time.Duration(ms) * time.Millisecond)
-}
-
-func parseArgs(args map[string]interface{}) (options, error) {
-
-	rewindLines := int(0)
-	delayMilliseconds := int(250)
-	filePath := ""
-
-	if rewindArg, ok := args["--rewind"].(string); ok {
-		rewindLines, _ = strconv.Atoi(rewindArg)
-	}
-
-	if rewindLines < 0 || rewindLines > math.MaxInt32 {
-		return options{}, errors.New("--rewind must be a positive number of lines")
-	}
-
-	if delayArg, ok := args["--delay"].(string); ok {
-		delayMilliseconds, _ = strconv.Atoi(delayArg)
-	}
-
-	if delayMilliseconds < 0 || delayMilliseconds > math.MaxInt32 {
-		return options{}, errors.New("--delay must be a positive number of milliseconds")
-	}
-
-	if filePathArg, ok := args["<file>"].(string); ok {
-		filePath = filePathArg
-	}
-
-	return options{
-		rewindLines,
-		delayMilliseconds,
-		filePath,
-	}, nil
-}
-
-func stdinToChan(source io.Reader, linesChannel *chan string, rewindLinesCount int) {
-	readline.ReadLine(os.Stdin, func(line string) {
-		*linesChannel <- line
-
-		if rewindLinesCount == 0 {
-			sleepMilliseconds(globalDelay)
-		}
-
-		if rewindLinesCount > 0 {
-			rewindLinesCount--
-		}
-	})
-
-	close(*linesChannel)
-}
-
-func fileToChan(source string, linesChannel *chan string, rewindLinesCount int) {
-	if rewindLinesCount > 0 {
-		tailFile(source, rewindLinesCount)
-	}
-
-	t, err := tail.TailFile(source, tail.Config{
-		Follow:   true,
-		Poll:     true,
-		Location: &tail.SeekInfo{Offset: 0, Whence: 2},
-		Logger:   tail.DiscardingLogger,
-	})
-
-	if err == nil {
-		for line := range t.Lines {
-			*linesChannel <- line.Text
-			sleepMilliseconds(globalDelay)
-		}
-	} else {
-		close(*linesChannel)
-		checkErr(err)
-	}
-}
-
-func main() {
-	arguments, _ := docopt.Parse(doc, nil, true, version, false)
-
-	options, err := parseArgs(arguments)
-
-	if err != nil {
-		checkErr(err)
-	}
-
-	globalDelay = options.delayMilliseconds
-
-	if len(options.filePath) > 0 {
-		linesChannel := make(chan string, 1)
-
-		if isStdinPath(options.filePath) {
-			go stdinToChan(os.Stdin, &linesChannel, options.rewindLines)
-		} else {
-			go fileToChan(options.filePath, &linesChannel, options.rewindLines)
-		}
-
-		for line := range linesChannel {
-			fmt.Println(line)
-		}
-	}
 }
